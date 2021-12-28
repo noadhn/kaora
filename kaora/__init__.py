631b75f06516d7c7f04967698f7db360c01291b2
noadhn <noasworking@gmail.com>
2021-12-28 20:17:01 +0200

    initial deploy

--- kaora/__init__.py
+++ kaora/__init__.py
@@ -2,10 +2,20 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from kaora import app
+
+ENV = "prod"
+if ENV == "prod":
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        'postgres://nhbshaqbygmaef:67039998f60f17f686c2e841b6eb62c86d3f582ef4ff9237ffb1729f70ef7dc2@ec2-3-213-76-170.compute-1.amazonaws.com:5432/d1fsdabj56c882'
+else:
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        'postgresql://postgres:postgres@localhost/flasksql'
 
 app = Flask(__name__, template_folder='template')
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/flasksql'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['APPLICATION_ROOT'] = False
 app.config['OAUTHLIB_INSECURE_TRANSPORT'] = True
